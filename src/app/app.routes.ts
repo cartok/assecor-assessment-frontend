c76fe95d44a19642c30f9d0c259533d54710a0e0
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-23 12:01:42 +0100

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: 'movies',
+    loadComponent: () =>
+      import('./pages/movies/movies.component').then(
+        (component) => component.MoviesComponent,
+      ),
+  },
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./pages/characters/characters.component').then(
+        (component) => component.CharactersComponent,
+      ),
+  },
+  {
+    path: 'planets',
+    loadComponent: () =>
+      import('./pages/planets/planets.component').then(
+        (component) => component.PlanetsComponent,
+      ),
+  },
+]
---
+++ src/app/pages/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-characters',
+  imports: [],
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.css',
+})
+export class CharactersComponent {}
