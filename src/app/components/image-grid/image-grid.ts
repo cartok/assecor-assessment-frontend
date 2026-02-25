582a41334397f94659024c340b3b6eb970690687
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-25 16:48:26 +0100

    wip resource grid item: render data

--- src/app/components/image-grid/image-grid.ts
+++ src/app/components/image-grid/image-grid.ts
@@ -1,7 +1,4 @@
-import { Component, input } from '@angular/core'
-
-import type { ImageGridItem } from '@/components/image-grid/image-grid-item/image-grid-item'
-import type { InputValues } from '@/shared/types/component.types'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-image-grid',
@@ -9,9 +6,4 @@ import type { InputValues } from '@/shared/types/component.types'
   templateUrl: './image-grid.html',
   styleUrl: './image-grid.css',
 })
-export class ImageGrid {
-  readonly items =
-    input.required<
-      InputValues<typeof ImageGridItem, 'imageUrl' | 'label' | 'linkLabel' | 'imageAlt'>[]
-    >()
-}
+export class ImageGrid {}
--- src/app/pages/movies/movies.ts
+++ src/app/pages/movies/movies.ts
@@ -1,10 +1,9 @@
-import { Component, computed, inject, signal } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
 
 import { FilmsService } from '@/api/swapi/resources/films/films.service'
 import { ImageGrid } from '@/components/image-grid/image-grid'
 import { ImageGridItem } from '@/components/image-grid/image-grid-item/image-grid-item'
 import { PageHeading } from '@/components/page-heading/page-heading'
-import type { InputValue } from '@/shared/types/component.types'
 
 @Component({
   selector: 'app-movies',
@@ -16,12 +15,4 @@ export class Movies {
   readonly filmsService = inject(FilmsService)
   readonly page = signal('1')
   readonly collection = this.filmsService.getCollection(this.page)
-  readonly gridItems = computed<InputValue<typeof ImageGrid, 'items'>>(() =>
-    this.collection.data().items.map((film) => ({
-      imageUrl: film.images[0],
-      imageAlt: `Image of "${film.title}"`,
-      label: film.title,
-      linkLabel: `Go to "${film.title}" detail page`,
-    })),
-  )
 }
