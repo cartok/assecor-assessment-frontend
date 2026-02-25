74d5924fda6eca592587ed138f3a28ee51a2c499
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-25 23:08:29 +0100

    wip on image slider

--- src/app/components/image-slider/image-slider.ts
+++ src/app/components/image-slider/image-slider.ts
@@ -1,4 +1,5 @@
-import { Component, input, signal } from '@angular/core'
+import type { ElementRef } from '@angular/core'
+import { Component, effect, input, signal, viewChild } from '@angular/core'
 
 @Component({
   selector: 'app-image-slider',
@@ -11,4 +12,20 @@ export class ImageSlider {
     input.required<{ url: string; alt: string; width: number; height: number }[]>()
   readonly viewportHeight = input.required<number>()
   activeIndex = signal(0)
+
+  readonly viewport = viewChild.required<ElementRef<HTMLElement>>('viewport')
+  readonly width = signal(0)
+  constructor() {
+    effect((onCleanup) => {
+      const el = this.viewport().nativeElement
+      this.width.set(el.getBoundingClientRect().width)
+
+      const observer = new ResizeObserver(([entry]) => {
+        this.width.set(entry.contentRect.width)
+      })
+
+      observer.observe(el)
+      onCleanup(() => observer.disconnect())
+    })
+  }
 }
