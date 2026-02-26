f4f33bdf71ae286cffc4cd885f826ef0e6a21a13
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-02-26 18:15:52 +0100

    implemen the new persons service into the characters page

--- src/app/pages/characters/characters.ts
+++ src/app/pages/characters/characters.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
+
+import type { Person } from '@/api/swapi/resources/people/people.model'
+import { PeopleService } from '@/api/swapi/resources/people/people.service'
+import { ImageGrid } from '@/components/image-grid/image-grid'
+import { ImageGridItem } from '@/components/image-grid/image-grid-item/image-grid-item'
+import { PageHeading } from '@/components/page-heading/page-heading'
 
 @Component({
   selector: 'app-characters',
-  imports: [],
+  imports: [PageHeading, ImageGrid, ImageGridItem],
   templateUrl: './characters.html',
   styleUrl: './characters.css',
 })
-export class Characters {}
+export class Characters {
+  readonly peopleService = inject(PeopleService)
+  readonly page = signal('1')
+  readonly collection = this.peopleService.getCollection(this.page)
+
+  imageTitle = (person: Person) => `Image of "${person.name}"`
+  linkUri = (person: Person) => `/character/${person.id}`
+}
