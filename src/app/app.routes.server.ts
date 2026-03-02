491c4f268504c594f0448d1724aded8145e2fa78
Dennis Weiershaeuser <dennis.weiershaeuser@gmail.com>
2026-03-02 16:08:17 +0100

    add static routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,14 @@ import { RenderMode } from '@angular/ssr'
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'error',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server,
   },
